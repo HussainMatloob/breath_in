b96ef27861f06c4536fc1b4328d4d92eda33af42
Hussain Matloob <hussainmatloob98012@gmail.com>
2024-10-03 13:23:18 -0700

    minor change

--- lib/views/screens/sleep_screen.dart
+++ lib/views/screens/sleep_screen.dart
@@ -1,4 +1,9 @@
+import 'package:breath_in/constants/color_constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import '../custom_widgets/custom_button_widget.dart';
 
 class SleepScreen extends StatefulWidget {
   const SleepScreen({super.key});
@@ -10,6 +15,24 @@ class SleepScreen extends StatefulWidget {
 class _SleepScreenState extends State<SleepScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.only(left: 20.w,top: 20.h),
+          child: CustomButton(
+            width: 39.w,
+            height: 39.h,
+            bordercircular: 10.r,
+            icon: Icon(Icons.arrow_back_ios_new,color: ColorConstant.blackColor,size: 20.sp,),
+            onTap: (){
+              Get.back();
+            },
+            borderColor:ColorConstant.borderColor,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+    );
   }
 }
